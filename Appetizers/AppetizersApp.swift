e3a97d2b3a7e7fd3f6685cb173fe082570510604
Maso Mahboob <masomahboob@proton.me>
2023-11-09 15:05:32 +0430

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mosawer Mahboob on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
