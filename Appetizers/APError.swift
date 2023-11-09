d1d5a022de3b86789c2f5823415be2a3bf224e59
Maso Mahboob <masomahboob@proton.me>
2023-11-09 18:53:01 +0430

    Create a NetworkManager, ask for data from the server and decode the JSON.

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Mosawer Mahboob on 11/9/23.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
